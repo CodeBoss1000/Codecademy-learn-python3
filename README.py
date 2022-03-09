f53bd404e15bfdd0aa61fae5cd1202727da0d41e
CodeBoss1000 <100922415+CodeBoss1000@users.noreply.github.com>
2022-03-09 00:36:52 +0000

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,12 @@
+#RJ
+#fun fact: I love the SIMS 4
+
+print("    RRRRR          JJJJJ   ")
+print("    R   R            J     ")
+print("    R   R            J     ")
+print("    RRRR             J     ")
+print("    R R              J     ")
+print("    R  R           J J     ")
+print("    R   R          J J     ")
+print("    R    R          JJ     ")
+
